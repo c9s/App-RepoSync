31c39bc7fe1025d63e5eab9fd416e1ab86e03327
c9s <cornelius.howl@gmail.com>
2012-01-12 23:38:07 +0800

    Add repo

--- lib/App/RepoSync.pm
+++ lib/App/RepoSync.pm
@@ -3,6 +3,11 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+
+
+
+
+
 1;
 __END__
 
---
+++ lib/App/RepoSync/Command/Export.pm
@@ -0,0 +1,37 @@
+package App::RepoSync::Command::Export;
+use 5.10.0;
+use warnings;
+use strict;
+use base qw( CLI::Framework::Command );
+use Cwd;
+use YAML;
+use App::RepoSync::Export;
+
+
+sub run {
+    my ($self,$opts,@args) = @_;
+
+    my ($export_file,@dirs) = @args;
+
+    $export_file ||= 'repos.yml';
+
+    @dirs = getcwd() unless @dirs;
+
+    say 'scanning repos...';
+    my @data = ();
+    for( @dirs ) {
+        my @repos = App::RepoSync::Export->run( $_ );
+        push @data, @repos;
+    }
+
+    YAML::DumpFile( $export_file , {
+        version => 0.1,
+        repos => \@data,
+    });
+
+    say "done. @{[ scalar @data ]} repositories exported.";
+}
+
+
+
+1;
